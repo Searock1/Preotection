8bf60dd41eefa0358a08d9379f653ce401b7836b
Searock1 <145963314+Searock1@users.noreply.github.com>
2024-04-25 21:10:34 +0600

    config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     app: {
         px: '+',
-        token: '',
+        token: 'MTIwNzM0MjMxMzUxMzQxODgzMg.G4Lg1x.fkMEGZ-7ETKxz3_2IEl-a9Ikq0_JgjsAkL4x6Q',
         playing: '',
         funny: '',
         support: "",
